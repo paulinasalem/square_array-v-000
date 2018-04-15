559759047cac31c4442d80b2f1eec12e3c20b4cc
Paulina Salem <paulinasalem@yahoo.com>
2018-04-15 17:25:16 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,5 @@
 def square_array(array)
-array.each {|number| numbers**2}
-new_numbers=array.each {|number| numbers**2}
+array.each {|number| number**2}
+new_numbers=array.each {|number| number**2}
 new_numbers
 end
